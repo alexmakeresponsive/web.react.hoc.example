bbfd633f34ddc430a318237b59d0062a80f7ff2b
Alexandr Gorchakov <alexmakeresponsive@mail.ru>
2018-07-13 23:22:05 +0600

    PostEditor: add button disable behavior

--- client/src/components/PostEditor/index.jsx
+++ client/src/components/PostEditor/index.jsx
@@ -12,38 +12,40 @@ class PostEditor extends Component {
             value: '',
             status: null
         },
-        // classNameSuccess: 'success',
-        // classNameError:   'error'
 
+        submitIsActive: {
+            status: null
+        }
     };
 
-    validationMessage1Container = null;
-    validationMessage2Container = null;
-
-    getValidationMessage1Ref = (element) => {
-        this.validationMessage1Container = element;
-    };
-    getValidationMessage2Ref = (element) => {
-        this.validationMessage2Container = element;
+    consts = {
+        titleAttributesName: 'titleFormPostEditor',
+        textAttributesName:  'textFormPostEditor'
     };
 
-    titleFormPostEditorValidation = (e) => {
+    FormPostEditorValidation = (e) => {
+        let dataStateName = e.target.attributes['data-state-name'].value;
+        let textLength = (dataStateName === 'titleFormPostEditor') ? 10 : 20;
+
         this.setState({
-            titleFormPostEditor: {
-                value:             e.target.value,
-                status: (e.target.value.length < 10) ? false : true
+            [dataStateName]: {
+                value:   e.target.value,
+                status: (e.target.value.length < textLength) ? false : true
             }
         });
 
-    };
-    textFormPostEditorValidation = (e) => {
-        this.setState({
-            textFormPostEditor: {
-                value:             e.target.value,
-                status: (e.target.value.length < 20) ? false : true
-            }
-        });
-        // showValidationStatus(validationMessage2Container, this.state.textFormPostEditor.status );
+
+        // console.log('FormPostEditorValidation start');
+        //&& this.state.textFormPostEditor.status
+        if (this.state.titleFormPostEditor.status) {
+            console.log('enable button');
+            //button disable
+            this.setState({
+                submitIsActive: {
+                    status: true
+                }
+            });
+        }
     };
 
     addValidationClassName = ( status ) => {
@@ -59,13 +61,32 @@ class PostEditor extends Component {
         }
     };
 
-    setValidationMessage = ( status ) => {
+    addActiveClassName = (status) => {
+        if (status) {
+            return 'active';
+        }
+
+        return 'disabled';
+    };
+
+    setValidationMessage = ( status, name ) => {
         switch (status) {
             case true:
                 return <span>Success</span>;
                 break;
             case false:
-                return 'Need more then 10 letters';
+                switch (name) {
+                    case 'titleFormPostEditor':
+                        // console.log('name title ? = ', name);
+                        return 'Need more then 10 letters';
+                        break;
+                    case 'textFormPostEditor':
+                        // console.log('name text ? = ', name);
+                        return 'Need more then 20 letters';
+                        break;
+                    default:
+                        return 'Text not corrected';
+                }
                 break;
             default:
                 return '';
@@ -73,6 +94,16 @@ class PostEditor extends Component {
     };
 
     render = () => {
+        let stateTitle = this.state.titleFormPostEditor;
+        let stateText  = this.state.textFormPostEditor;
+        let stateSubmit  = this.state.submitIsActive;
+
+        let titleName = this.consts.titleAttributesName;
+        let textName = this.consts.textAttributesName;
+
+        // console.log(this.state.titleFormPostEditor.status);
+        // console.log(this.state);
+
         return (
             <div className="PostEditor">
                 <h1 className="titleComponent">Posteditor</h1>
@@ -82,33 +113,39 @@ class PostEditor extends Component {
                             <legend>Add new post</legend>
                             <div className="inputWr">
                                 <label htmlFor="title">Title</label>
-                                <textarea id="titleFormPostEditor"
+                                <textarea id={titleName}
                                           placeholder="title"
-                                          value={this.state.titleFormPostEditor.value}
-                                          onChange={this.titleFormPostEditorValidation}
+                                          data-state-name={titleName}
+                                          value={stateTitle.value}
+                                          onChange={this.FormPostEditorValidation}
                                 />
                                 <span className={"validationMessage " +
-                                                  this.addValidationClassName(this.state.titleFormPostEditor.status )}
-                                      ref={this.getValidationMessage1Ref} >
-                                    {this.setValidationMessage(this.state.titleFormPostEditor.status)}
+                                                  this.addValidationClassName(stateTitle.status )} >
+                                    {this.setValidationMessage(stateTitle.status, titleName)}
                                 </span>
 
                             </div>
                             <div className="inputWr">
                                 <label htmlFor="text">Text</label>
-                                <textarea id="textFormPostEditor"
+                                <textarea id={textName}
                                           placeholder="text"
-                                          value={this.state.textFormPostEditor.value}
-                                          onChange={this.textFormPostEditorValidation}
+                                          data-state-name={textName}
+                                          value={stateText.value}
+                                          onChange={this.FormPostEditorValidation}
                                 />
-                                <span className={"validationMessage "}
-                                      ref={this.getValidationMessage2Ref} />
+                                <span className={"validationMessage " +
+                                                  this.addValidationClassName(stateText.status )}>
+                                    {this.setValidationMessage(stateText.status, textName)}
+                                </span>
                             </div>
                             <div>
-                                <button type="submit" className="primary-2">
-                                    Submit
-                                </button>
+
                             </div>
+                            <button type="submit"
+                                    className={"primary-2 " +
+                                    this.addActiveClassName(stateSubmit.status)}>
+                                Submit
+                            </button>
                         </fieldset>
                     </form>
 
